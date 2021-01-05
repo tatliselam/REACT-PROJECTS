00a91cb9f70529e186fe9db3133656985b61046d
Abduselam Tatli <a.selamtatli@Abduselams-MacBook-Pro.local>
2021-01-04 22:51:33 -0800

    complete 3rd Day css project

---
+++ React 03 Projects/06-CSS_Project/src/components/App.js
@@ -0,0 +1,10 @@
+import "./App.css"
+function App (){
+return (
+<div className ='parent'>
+<h1>“Hello World”</h1>
+<h2>“How are you?"</h2>
+<p id = 'parap' >"I m fine"</p>
+</div>
+)}
+export default App;
